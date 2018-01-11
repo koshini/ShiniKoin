04d52cf339ad96fedce83b44155dcc496f06a9f8
Shini <ko.shmily@gmail.com>
2018-01-11 16:24:38 -0500

    Simple blockchain of 20 blocks

---
+++ logging_helper.py
@@ -0,0 +1,17 @@
+import logging
+
+
+def setup_logger():
+    # logging.basicConfig(format="%(asctime)s  %(levelname)s \t %(name)s \t %(message)s", level=logging.DEBUG)
+    # logging_format = "%(levelname)-8s %(name)-40s %(funcName)-30s %(message)-150s %(asctime)s"
+    logging_format = "%(levelname)-8s %(name)-20s %(funcName)-20s %(message)-150s"
+
+    # Log to console:
+    logging.basicConfig(format=logging_format, level=logging.INFO)
+
+    # Log to file:
+    # logging.basicConfig(format=logging_format, level=logging.WARNING, filename="app.log", filemode="w")
+
+    logger = logging.getLogger('elasticsearch')
+    logger.setLevel(logging.WARNING)
+
